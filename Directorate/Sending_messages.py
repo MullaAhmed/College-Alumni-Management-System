eb6bea2e041da76ab11bb6fc9dd620912ae096fc
MullaAhmed <67974888+MullaAhmed@users.noreply.github.com>
2021-08-29 07:02:27 +0530

    Update Sending_messages.py

--- Directorate/Sending_messages.py
+++ Directorate/Sending_messages.py
@@ -1,7 +1,7 @@
 def send_message(message):
     from twilio.rest import Client
 
-    client = Client("ACe2ef31a4d09f9846afcd486586495973", "f157fdaab821b857ecb305a7027a96f8")
+    client = Client("ACe2ef31a4d09f9846afcd486586495973", "8d2e36f767db9c4948d5ad81a03e933d")
 
     numbers=["+917202964175","+918690909695"]
     names=["Hardeep","Ahmed"]
